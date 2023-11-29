bee42698295ff3ab108f8a0b01ba0b9db337d75b
sabine-thb <sabine.thibout@edu.univ-eiffel.fr>
2023-11-29 12:23:20 +0100

    responsive

--- accueil.php
+++ accueil.php
@@ -37,13 +37,13 @@ $utilisateur=$_SESSION['utilisateur'];
 	<a href="#" class="toggle-menu" data-toggle-class="active" data-toggle-target=".main-menu, this"><svg height="32" viewBox="0 0 32 32" width="32" xmlns="http://www.w3.org/2000/svg"><path d="M4 10h24c1.104 0 2-.896 2-2s-.896-2-2-2H4c-1.104 0-2 .896-2 2s.896 2 2 2zm24 4H4c-1.104 0-2 .896-2 2s.896 2 2 2h24c1.104 0 2-.896 2-2s-.896-2-2-2zm0 8H4c-1.104 0-2 .896-2 2s.896 2 2 2h24c1.104 0 2-.896 2-2s-.896-2-2-2z" fill="#fff"/></svg></a>
 	
     <ul class="main-menu">
-        <li><a href="#"><span>Mon profil</span></a> 
+        <li><div><span>Mon profil</span></div> 
             <ul class="sub-menu">
                 <li><a href="absences.php"><span>Absences et retards</span></a></li>
                 <li><a href="notes.php"><span>Notes</span></a></li>
             </ul>
         </li>
-        <li><a href="#"><span>Scolarité</span></a>
+        <li><div><span>Scolarité</span></div>
             <ul class="sub-menu">
                 <li><a href="edt.php"><span>Emploi du temps</span></a></li>
                 <li><a href="devoirs.php"><span>Devoirs à rendre</span></a></li>
@@ -51,13 +51,13 @@ $utilisateur=$_SESSION['utilisateur'];
                 <li><a href="infosProf.php"><span>Informations sur les professeurs</span></a></li>
             </ul>
         </li>
-        <li><a href="#"><span>Services</span></a>
+        <li><div><span>Services</span></div>
             <ul class="sub-menu">
                 <li><a href="restauration.php"><span>Restauration</span></a></li>
                 <li><a href="resa.php"><span>Réservation de matériel</span></a></li>
             </ul>
         </li>
-        <li><a href="#"><span>Informations</span></a>
+        <li><div><span>Informations</span></div>
             <ul class="sub-menu">
                 <li><a href="actus.php"><span>Actualités étudiantes</span></a></li>
                 <li><a href="carte.php"><span>Carte du campus</span></a></li>
