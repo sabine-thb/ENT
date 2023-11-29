2b548c8188cb74d1c6618b0dc6dfd2b5a33affea
sabine-thb <sabine.thibout@edu.univ-eiffel.fr>
2023-11-29 01:24:14 +0100

    responsiveNav

--- accueil.php
+++ accueil.php
@@ -1,12 +1,84 @@
-<h1>Bienvenue sur l'accueil de l'ENT </h1>
 <?php
 session_start();
-
 $utilisateur=$_SESSION['utilisateur'];
-
-echo"Bonjour et bienvenue {$utilisateur ['prenom']} "
 ?>
 
-<form action="deconnexion.php">
-    <input type="submit" value="Se déconnecter">
-</form>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+    <link rel="stylesheet" href="fonts.css">
+    <link rel="stylesheet" href="burger.css">
+   <link rel="stylesheet" href="styleAccueil.css">
+   
+    
+    <title>Accueil ENT</title>
+</head>
+<body>
+    <!-- <nav>
+        <a href="accueil.php" class="logo"></a>
+        <a href="monProfil.php" class="navItem">Mon profil</a>
+        <a href="scolarité.php" class="navItem">Scolarité</a>
+        <a href="services.php" class="navItem">Services</a>
+        <a href="informations.php" class="navItem">informations</a>
+        <a href="deconnexion.php" class="navItem">
+            <img src="./img/deco.svg"  class="deco" alt="se déconnecter">
+        </a>
+    </nav> -->
+    
+</head>
+<body>
+<header>
+	<div class="logo"></div>
+
+<nav class="responsive-menu">
+	<a href="#" class="toggle-menu" data-toggle-class="active" data-toggle-target=".main-menu, this"><svg height="32" viewBox="0 0 32 32" width="32" xmlns="http://www.w3.org/2000/svg"><path d="M4 10h24c1.104 0 2-.896 2-2s-.896-2-2-2H4c-1.104 0-2 .896-2 2s.896 2 2 2zm24 4H4c-1.104 0-2 .896-2 2s.896 2 2 2h24c1.104 0 2-.896 2-2s-.896-2-2-2zm0 8H4c-1.104 0-2 .896-2 2s.896 2 2 2h24c1.104 0 2-.896 2-2s-.896-2-2-2z" fill="#fff"/></svg></a>
+	
+    <ul class="main-menu">
+        <li><a href="#"><span>Mon profil</span></a> 
+            <ul class="sub-menu">
+                <li><a href="absences.php"><span>Absences et retards</span></a></li>
+                <li><a href="notes.php"><span>Notes</span></a></li>
+            </ul>
+        </li>
+        <li><a href="#"><span>Scolarité</span></a>
+            <ul class="sub-menu">
+                <li><a href="edt.php"><span>Emploi du temps</span></a></li>
+                <li><a href="devoirs.php"><span>Devoirs à rendre</span></a></li>
+                <li><a href="cours.php"><span>Cours</span></a></li>
+                <li><a href="infosProf.php"><span>Informations sur les professeurs</span></a></li>
+            </ul>
+        </li>
+        <li><a href="#"><span>Services</span></a>
+            <ul class="sub-menu">
+                <li><a href="restauration.php"><span>Restauration</span></a></li>
+                <li><a href="resa.php"><span>Réservation de matériel</span></a></li>
+            </ul>
+        </li>
+        <li><a href="#"><span>Informations</span></a>
+            <ul class="sub-menu">
+                <li><a href="actus.php"><span>Actualités étudiantes</span></a></li>
+                <li><a href="carte.php"><span>Carte du campus</span></a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="deconnexion.php" class="navItem">
+                <img src="./img/deco.svg"  class="deco" alt="se déconnecter">
+            </a> 
+        </li>
+    </ul>
+</nav>
+	
+</header>
+    <section class="section1">
+        <?php
+        echo"<h1>Bonjour {$utilisateur ['prenom']}</h1> "
+        ?>
+        <p>Bienvenue sur l’espace numérique de travail de l’université Gustave Eiffel.</p>
+</section>
+    
+<script src="./script/burger.js"></script>
+</body>
+</html>
