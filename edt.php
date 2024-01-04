5b24386fe62b9aa8d83d17cd10488d164f6e0667
Jade Célestine <124267211+jadecelestine@users.noreply.github.com>
2024-01-04 11:31:19 +0100

    icon + edt

---
+++ edt.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <link rel="stylesheet" href="style/style.css">   
+    <link rel="stylesheet" href="style/fonts.css">
+    <link rel="stylesheet" href="style/burger.css">
+    <link rel="stylesheet" href="style/styleAccueil.css">
+    <title>Emploi du Temps</title>
+</head>
+<body>
+    
+</head>
+<body>
+    <header>
+        <a href="accueil.php" class="logo"></a>
+
+        <nav class="responsive-menu">
+            <a href="#" class="toggle-menu" data-toggle-class="active" data-toggle-target=".main-menu, this"><svg height="32" viewBox="0 0 32 32" width="32" xmlns="http://www.w3.org/2000/svg"><path d="M4 10h24c1.104 0 2-.896 2-2s-.896-2-2-2H4c-1.104 0-2 .896-2 2s.896 2 2 2zm24 4H4c-1.104 0-2 .896-2 2s.896 2 2 2h24c1.104 0 2-.896 2-2s-.896-2-2-2zm0 8H4c-1.104 0-2 .896-2 2s.896 2 2 2h24c1.104 0 2-.896 2-2s-.896-2-2-2z" fill="#fff"/></svg></a>
+            
+            <ul class="main-menu">
+                <li><div class="navFirstLevel"><span>Mon profil</span></div> 
+                    <ul class="sub-menu">
+                        <li><a href="absences.php"><span>Absences et retards</span></a></li>
+                        <li><a href="notes.php"><span>Notes</span></a></li>
+                    </ul>
+                </li>
+                <li><div class="navFirstLevel"><span>Scolarité</span></div>
+                    <ul class="sub-menu">
+                        <li><a href="edt.php"><span>Emploi du temps</span></a></li>
+                        <li><a href="devoirs.php"><span>Devoirs à rendre</span></a></li>
+                        <li><a href="cours.php"><span>Cours</span></a></li>
+                    </ul>
+                </li>
+                <li><div class="navFirstLevel"><span>Services</span></div>
+                    <ul class="sub-menu">
+                        <li><a href="restauration.php"><span>Restauration</span></a></li>
+                        <li><a href="resa.php"><span>Réservation de matériel</span></a></li>
+                    </ul>
+                </li>
+                <li><div class="navFirstLevel"><span>Informations</span></div>
+                    <ul class="sub-menu">
+                        <li><a href="infosProf.php"><span>Informations sur les professeurs</span></a></li>
+                        <li><a href="actus.php"><span>Actualités étudiantes</span></a></li>
+                        <li><a href="carte.php"><span>Carte du campus</span></a></li>
+                    </ul>
+                </li>
+                <li>
+                    <div class="navFirstLevel">
+                        <a href="deconnexion.php" class="navItem">
+                        <img src="./style/img/deco.svg"  class="deco" alt="se déconnecter">
+                        </a> 
+                </div>
+                    
+                </li>
+            </ul>
+        </nav>
+        
+    </header>
+
+    <section class="section1">
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FParis&showTitle=0&showNav=1&showPrint=0&showTabs=1&showCalendars=0&showTz=1&mode=WEEK&src=dW5kNXE3ZDczbW1nbWkybjZra2Vub2RjaTVtbGhqOGpAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23D50000" style="border:solid 1px #777" width="900" height="600" frameborder="0" scrolling="no"></iframe>
+    </section>
+        
+    <script src="./script/burger.js"></script>
+
+    <a href="messagerie.php" class="chat"><img src="./style/img/chat.png" alt="Bouton vers la messagerie" srcset="" class="imgChat"></a>
+
+</body>
+</html>
