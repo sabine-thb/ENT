df71cff7c4724ef6c8c192cd4f4c89b99ed2fc27
Jade Célestine <124267211+jadecelestine@users.noreply.github.com>
2024-01-04 12:28:42 +0100

    edt

--- edt.php
+++ edt.php
@@ -60,7 +60,7 @@
     </header>
 
     <section class="section1">
-        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FParis&showTitle=0&showNav=1&showPrint=0&showTabs=1&showCalendars=0&showTz=1&mode=WEEK&src=dW5kNXE3ZDczbW1nbWkybjZra2Vub2RjaTVtbGhqOGpAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23D50000" style="border:solid 1px #777" width="900" height="600" frameborder="0" scrolling="no"></iframe>
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FParis&showTitle=0&showPrint=0&showTabs=1&showCalendars=0&mode=WEEK&showDate=1&showNav=1&src=dW5kNXE3ZDczbW1nbWkybjZra2Vub2RjaTVtbGhqOGpAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%234549ae" width="50%" height="600" frameborder="0" scrolling="no"></iframe>
     </section>
         
     <script src="./script/burger.js"></script>
